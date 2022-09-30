7ac5fb7f601c0ba02da4256e7c86b8c60bef08ef
yms213 <dbalstjd789@daum.net>
2022-09-30 17:55:11 +0900

    stussy clone

---
+++ src/test/java/com/stussy/stussyclone20220930yms/StussyClone20220930YmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220930yms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussyClone20220930YmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
