5f8b1d342710a8f6707ef56e9aab84ed0c8f730a
yms213 <dbalstjd789@daum.net>
2022-10-04 17:16:10 +0900

    데이터연결

--- src/main/java/com/stussy/stussyclone20220930yms/controller/AccountController.java
+++ src/main/java/com/stussy/stussyclone20220930yms/controller/AccountController.java
@@ -1,17 +1,19 @@
-package com.stussy.stussyclone20220930yms;
+package com.stussy.stussyclone20220930yms.controller;
 
+import com.stussy.stussyclone20220930yms.dto.RegisterReqDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ControllerPage {
+public class AccountController {
 
     @GetMapping ("/account/login")
     public String login(){
         return "account/login";
     }
     @GetMapping("/account/register")
-    public String register(){
+    public String register(RegisterReqDto registerReqDto){
+        System.out.println();
         return "account/register";
     }
 }
