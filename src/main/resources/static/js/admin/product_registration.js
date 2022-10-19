e906ac25c9beb9d1639ae4aace6f162256a0c7eb
yms213 <dbalstjd789@daum.net>
2022-10-19 13:58:17 +0900

    오류수정

--- src/main/resources/static/js/admin/product_registration.js
+++ src/main/resources/static/js/admin/product_registration.js
@@ -1,120 +1,111 @@
+// 상품종류 선택 시 상품명 입력 가능하게 하는 js코드
 const RegisterEventService = {
-    getCategorySelectObj: () => document.querySelectorAll(".product-inputs")[0],
-    getNameInputObj: () => document.querySelectorAll(".product-inputs")[1],
-    getPriceInputObj: () => document.querySelectorAll(".product-inputs")[2],
-    getRegistInfo: ()=>document.querySelector(".regist-info"),
-    getRegisterButtonObj: ()=>document.querySelector(".regist-button"),
-    getInfoTextareaObjs: ()=> document.querySelectorAll(".product-inputs"),
+  getCategorySelectObj: () => document.querySelectorAll(".product-inputs")[0],
+  getNameInputObj: () => document.querySelectorAll(".product-inputs")[1],
+  getPriceInputObj: () => document.querySelectorAll(".product-inputs")[2],
+  getRegistInfo: () => document.querySelector(".regist-info"),
+  getRegistButtonObj: () => document.querySelector(".regist-button"),
+  getInfoTextareaObjs: () => document.querySelectorAll(".product-inputs"),
 
-    init: function() {
-        //초기상태는 상품명과 가격명의 박스가 비활성화 상태를 의미한다.
+  // init은 생성자의 뜻이며 자바에서 사용하는 this를 사용하기 위해선 익명클래스로 만들어 사용 해야한다.
+  init: function() {
+    this.getNameInputObj().disabled = true;
+    this.getPriceInputObj().disabled = true;
+    this.getRegistButtonObj().disabled = true;
+  },
+
+  addCategorySelectEvent: function() {
+    this.getCategorySelectObj().onchange = () => {
+      if(this.getCategorySelectObj().value != "none") {
+        this.getNameInputObj().disabled = false;
+      }else {
         this.getNameInputObj().disabled = true;
+      }
+      RegisterObj.category = this.getCategorySelectObj().value;
+    }
+  },
+  addNameInputEvent: function() {
+    this.getNameInputObj().onkeyup = () => {
+      if(this.getNameInputObj().value.length != 0) {
+        this.getPriceInputObj().disabled = false;
+      }else {
         this.getPriceInputObj().disabled = true;
-        this.getRegisterButtonObj().disabled = true;
-    },
+      }
+      RegisterObj.name = this.getNameInputObj().value;
+    }
+  },
+  addPriceInputEvent: function() {
+    this.getPriceInputObj().onkeyup = () => { 
+      if(this.getPriceInputObj().value.length != 0) {
+      this.getRegistButtonObj().disabled = false;
+      this.getRegistInfo().classList.remove("regist-info-invisible");
+    }else {
+      this.getRegistButtonObj().disabled = true;
+      this.getRegistInfo().classList.add("regist-info-invisible");
+    }
+     RegisterObj.price = this.getPriceInputObj().value;
+    }
+  },
+  addRegistButtonEvent: function() {
+    this.getRegistButtonObj().onclick = () => {
+      RegisterObj.simpleInfo = this.getInfoTextareaObjs()[3].value;
+      RegisterObj.detailInfo = this.getInfoTextareaObjs()[4].value;
+      RegisterObj.optionInfo = this.getInfoTextareaObjs()[5].value;
+      RegisterObj.managementInfo = this.getInfoTextareaObjs()[6].value;
+      RegisterObj.shippingInfo = this.getInfoTextareaObjs()[7].value;
 
-    addCategorySelectEvent: function() {
-        this.getCategorySelectObj().onchange = () => {
-            //onchange 상자의 상태가 변화했을 때
-            if(this.getCategorySelectObj().value != "none"){
-                //상품종류카테고리의 상태가 none(아무것도 선택이 되지 않았을 때)이 아니면 실행된다.
-                this.getNameInputObj().disabled = false;
-                //상품명의 카테고리가 작성할 수 있도록 설정된다.
-            }else{
-                this.getNameInputObj().disabled = true;
-                //위의 조건에 맞지 않으면 작성 안되게 된다.
-            }
-            RegisterObj.category = this.getCategorySelectObj().value;
-        }
-    },
-    addNameInputEvent: function() {
-        this.getNameInputObj().onkeyup = () => {
-            //onkeyup 상품명의 박스에서 키를 땠을 떄
-            if(this.getNameInputObj().value.length != 0){
-                //박스안에 들어있는 내용이 길이가 0 이 아닐때 실행된다.
-                this.getPriceInputObj().disabled = false;
-                //위의 조건에 해당하면 가격의 박스가 활성화 된다.
-                
-            }else{
-                this.getPriceInputObj().disabled = true;
-                
-                //조건에 해당되지 않으면 박스가 비활성화 된다.
-            }
-            RegisterObj.name = this.getNameInputObj().value;
-        }
-    },
-    addPriceInputEvent: function() {
-        this.getPriceInputObj().onkeyup = () => {
-            if(this.getPriceInputObj().value.length != 0){
-                this.getRegistInfo().disabled = false;
-                this.getRegistInfo().classList.remove("regist-info-invisible")
-            }else{
-                this.getRegisterButtonObj().disabled = true;
-                this.getRegistInfo().classList.add("regist-info-invisible")
-            }
-            RegisterObj.price = this.getPriceInputObj().value;
-        }
-    },
-    addRegistButtonEvent: function() {
-        this.getRegisterButtonObj().onclick = () => {
-            RegisterObj.simpleINfo = this.getInfoTextareaObjs()[3].value;
-            RegisterObj.detailInfo = this.getInfoTextareaObjs()[4].value;
-            RegisterObj.optionInfo = this.getInfoTextareaObjs()[5].value;
-            RegisterObj.managementInfo = this.getInfoTextareaObjs()[6].value;
-            RegisterObj.shippingInfo = this.getInfoTextareaObjs()[7].value;
-            
-            console.log(RegisterObj);
-            
-            RegisterRequestService.createProductRequest();
-        }
-    },
+      console.log(RegisterObj);
+
+      RegisterRequestService.createProductRequest();
+    }
+  }
 }
 
 const RegisterRequestService = {
-    createProductRequest: () =>{
-        let responseResult = null;
+  createProductRequest: () => {
+    let responseResult = null;
 
-        $.ajax({
-            async:false,
-            type:"post",
-            url: "/api/admin/product",
-            contentType: "application/json",
-            data: JSON.stringify(RegisterObj),
-            dataType: "json",
-            success:(response)=>{
-                responseResult = response.data;
-            },
-            error:(error)=>{
-                console.log(error);
-            }
-        })
-        return responseResult;
-    }
+    $.ajax({
+      async: false,
+      type: "post",
+      url: "/api/admin/product",
+      contentType: "application/json",
+      data: JSON.stringify(RegisterObj),
+      dataType: "json",
+      success: (reponse) => {
+        responseResult = response.data;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+
+    return responseResult;
+
+  }
 }
-//초기값은 null로 설정하고 값을 입력하면 들어간다.
+
 const RegisterObj = {
-    category:null,
-    name:null,
-    price:null,
-    simpleINfo:null,
-    detailInfo:null,
-    optionInfo:null,
-    managementInfo:null,
-    shippingInfo:null
+    category: null,
+    name: null,
+    price: null,
+    simpleInfo: null,
+    detailInfo: null,
+    optionInfo: null,
+    managementInfo: null,
+    shippingInfo: null
 }
 
 const ProductRegistration = {
-    initRegisterEvent: ()=>{
-        RegisterEventService.init();
-        RegisterEventService.addCategorySelectEvent();
-        RegisterEventService.addNameInputEvent();
-        RegisterEventService.addPriceInputEvent();
-        RegisterEventService.addRegistButtonEvent();
-
-    }
+  initRegisterEvent: () => {
+    RegisterEventService.init();
+    RegisterEventService.addCategorySelectEvent();
+    RegisterEventService.addNameInputEvent();
+    RegisterEventService.addPriceInputEvent();
+    RegisterEventService.addRegistButtonEvent();
+  }
 }
 
-window.onload = () =>{
-    //onload 웹페이지에 로드가 완료되었을 때
-    ProductRegistration.initRegisterEvent();
+window.onload = () => {
+  ProductRegistration.initRegisterEvent();
 }
