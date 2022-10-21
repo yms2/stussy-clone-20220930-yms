ff74426345e21c8c8c6768dd1a8fcd575621fd6b
yms213 <dbalstjd789@daum.net>
2022-10-21 17:08:35 +0900

    10/21 오류수정

--- src/main/resources/static/js/admin/product_registration.js
+++ src/main/resources/static/js/admin/product_registration.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class ProductMst{
   #category;
   #name;
@@ -61,78 +60,10 @@ class ProductMst{
 
 class CommonApi{
   getCategoryList(){
-=======
-// 상품종류 선택 시 상품명 입력 가능하게 하는 js코드
-const RegisterEventService = {
-  getCategorySelectObj: () => document.querySelectorAll(".product-inputs")[0],
-  getNameInputObj: () => document.querySelectorAll(".product-inputs")[1],
-  getPriceInputObj: () => document.querySelectorAll(".product-inputs")[2],
-  getRegistInfo: () => document.querySelector(".regist-info"),
-  getRegistButtonObj: () => document.querySelector(".regist-button"),
-  getInfoTextareaObjs: () => document.querySelectorAll(".product-inputs"),
-
-  // init은 생성자의 뜻이며 자바에서 사용하는 this를 사용하기 위해선 익명클래스로 만들어 사용 해야한다.
-  init: function() {
-    this.getNameInputObj().disabled = true;
-    this.getPriceInputObj().disabled = true;
-    this.getRegistButtonObj().disabled = true;
-  },
-
-  addCategorySelectEvent: function() {
-    this.getCategorySelectObj().onchange = () => {
-      if(this.getCategorySelectObj().value != "none") {
-        this.getNameInputObj().disabled = false;
-      }else {
-        this.getNameInputObj().disabled = true;
-      }
-      RegisterObj.category = this.getCategorySelectObj().value;
-    }
-  },
-  addNameInputEvent: function() {
-    this.getNameInputObj().onkeyup = () => {
-      if(this.getNameInputObj().value.length != 0) {
-        this.getPriceInputObj().disabled = false;
-      }else {
-        this.getPriceInputObj().disabled = true;
-      }
-      RegisterObj.name = this.getNameInputObj().value;
-    }
-  },
-  addPriceInputEvent: function() {
-    this.getPriceInputObj().onkeyup = () => { 
-      if(this.getPriceInputObj().value.length != 0) {
-      this.getRegistButtonObj().disabled = false;
-      this.getRegistInfo().classList.remove("regist-info-invisible");
-    }else {
-      this.getRegistButtonObj().disabled = true;
-      this.getRegistInfo().classList.add("regist-info-invisible");
-    }
-     RegisterObj.price = this.getPriceInputObj().value;
-    }
-  },
-  addRegistButtonEvent: function() {
-    this.getRegistButtonObj().onclick = () => {
-      RegisterObj.simpleInfo = this.getInfoTextareaObjs()[3].value;
-      RegisterObj.detailInfo = this.getInfoTextareaObjs()[4].value;
-      RegisterObj.optionInfo = this.getInfoTextareaObjs()[5].value;
-      RegisterObj.managementInfo = this.getInfoTextareaObjs()[6].value;
-      RegisterObj.shippingInfo = this.getInfoTextareaObjs()[7].value;
-
-      console.log(RegisterObj);
-
-      RegisterRequestService.createProductRequest();
-    }
-  }
-}
-
-const RegisterRequestService = {
-  createProductRequest: () => {
->>>>>>> aa8145d12c9bccb4ef5b272c3df8267d5f22979c
     let responseResult = null;
 
     $.ajax({
       async: false,
-<<<<<<< HEAD
       type: "get",
       url: "/api/admin/product/category",
       dataType : "json",
@@ -158,12 +89,6 @@ class RegisterApi{
       url: "/api/admin/product",
       contentType: "application/json",
       data: JSON.stringify(productMst),
-=======
-      type: "post",
-      url: "/api/admin/product",
-      contentType: "application/json",
-      data: JSON.stringify(RegisterObj),
->>>>>>> aa8145d12c9bccb4ef5b272c3df8267d5f22979c
       dataType: "json",
       success: (reponse) => {
         responseResult = response.data;
@@ -174,7 +99,6 @@ class RegisterApi{
     });
 
     return responseResult;
-<<<<<<< HEAD
   }
 }
 class RegisterEventService{
@@ -305,33 +229,4 @@ class RegisterService {
 window.onload = () => {
   RegisterService.getInstance().getCategoryList();
   RegisterService.getInstance().setRegisterHeaderEvent();
-=======
-
-  }
-}
-
-const RegisterObj = {
-    category: null,
-    name: null,
-    price: null,
-    simpleInfo: null,
-    detailInfo: null,
-    optionInfo: null,
-    managementInfo: null,
-    shippingInfo: null
-}
-
-const ProductRegistration = {
-  initRegisterEvent: () => {
-    RegisterEventService.init();
-    RegisterEventService.addCategorySelectEvent();
-    RegisterEventService.addNameInputEvent();
-    RegisterEventService.addPriceInputEvent();
-    RegisterEventService.addRegistButtonEvent();
-  }
-}
-
-window.onload = () => {
-  ProductRegistration.initRegisterEvent();
->>>>>>> aa8145d12c9bccb4ef5b272c3df8267d5f22979c
 }
